cd0d71ef49fa24508c5c52caca31b8d0d9edc967
penggong <1079747870@qq.com>
2024-11-23 18:41:06 +0800

    修复random参数

--- docker/java/create_event.sh
+++ docker/java/create_event.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-random_value = 1
-service_a_ip=""
-service_b_ip=""
+rand_no=1
+service_a_ip="10.101.53.66"
+service_b_ip="10.109.201.219"
 
 log_name="c_event.log"
 
-rand_create (){
-    random_value=`cat /dev/urandom | LC_CTYPE=C tr -dc '0-9' | fold -w $1 |head -n 1`
+rand_create(){
+    rand_no=`cat /dev/urandom | LC_CTYPE=C tr -dc '0-9' | fold -w $1 |head -n 1`
 }
 
 forward_fortio_create(){
@@ -17,6 +17,7 @@ forward_fortio_create(){
 # 接收参数1 服务名，参数2 增长数量
 forward_oom (){
   curl "$1:8080/forwardGet?targetSvc=$2:8080&targetApi=oom?count=$3"
+  echo "$1:8080/forwardGet?targetSvc=$2:8080&targetApi=oom?count=$3"
 }
 
 log_print() {
@@ -27,64 +28,63 @@ log_print() {
 while 1>0
 do
   rand_create 2
-  echo $random_value
+  echo $rand_no
 
   #1
   # b sleep
-  if [ "$random_value" = "11"  ];then
+  if [ "$rand_no" = "11"  ];then
      forward_fortio_create 20 $service_a_ip details2 sleepGet?time=5000 50
-     log_print random_value
+     log_print rand_no
   fi
 
   #2
     # b-c sleep 3s
-  if [ "$random_value" = "22"  ];then
-     forward_fortio_create 20 $service_a_ip details2 forwardTo/sleep/3000 50
-     log_print random_value
+  if [ "$rand_no" = "22"  ];then
+     forward_fortio_create 20 $service_a_ip details2 forwardToC?targetType=sleep&targetApi=3000 50
+     log_print rand_no
   fi
 
   #3
   # oom增长,b和c同时增长
-  if [ "$random_value" = "33"  ];then
+  if [ "$rand_no" = "33"  ];then
      forward_oom $service_a_ip details2 2 &
      forward_oom $service_b_ip details3 3 &
      sleep 20s
-     log_print random_value
+     log_print rand_no
   fi
 
   #4
   #CPU b-c  次数3000
-  if [ "$random_value" = "44"  ];then
-     forward_fortio_create 20 $service_a_ip details2 forwardTo/createObjectLimitCount/3000 50
-     log_print random_value
+  if [ "$rand_no" = "44"  ];then
+     forward_fortio_create 20 $service_a_ip details2 forwardToC?targetType=createObjectLimitCount&targetApi=3000 50
+     log_print rand_no
   fi
 
   #5  
     #CPU b  次数3000
-  if [ "$random_value" = "55"  ];then
-     forward_fortio_create 20 $service_a_ip details2 createObjectLimitCount?count=3000 50
-     log_print random_value
+  if [ "$rand_no" = "55"  ];then
+     forward_fortio_create 20 $service_a_ip details2 createObjectLimitCount?counttargetApi=3000 50
+     log_print rand_no
   fi
 
   #6
   #net a->b-c ya
-  if [ "$random_value" = "66"  ];then
-     forward_fortio_create 20 $service_a_ip details2 forwardTo/sleep/200 500
-     log_print random_value
+  if [ "$rand_no" = "66"  ];then
+     forward_fortio_create 20 $service_a_ip details2 forwardToC?targetType=sleep&targetApi=200 500
+     log_print rand_no
   fi
 
   #7
   #net b-C ya
-  if [ "$random_value" = "77"  ];then
+  if [ "$rand_no" = "77"  ];then
      forward_fortio_create 20 $service_b_ip details3 sleep?time=200 500
-     log_print random_value
+     log_print rand_no
   fi
 
   #8
   #配置错误
-  if [ "$random_value" = "88"  ];then
-     #
-     echo "error"
+  if [ "$rand_no" = "88"  ];then
+     echo "88"
   fi
 
 done
